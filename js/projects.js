d9fb078b68e528eefd43e9f6659d8c7ce3d1fdf6
Zephyr-Jeon <rltjd1258@gmail.com>
2024-08-21 17:56:25 -0700

    feature: proejcts named by prompt input

--- js/projects.js
+++ js/projects.js
@@ -3,3 +3,12 @@ const el1 = document.getElementById('project-name-title');
 const el2 = document.getElementById('breadcrumb-project-name-title');
 el1.innerText = lang;
 el2.innerText = lang;
+
+const projectNames = [];
+
+while (projectNames.length < 5) {
+  const input = window.prompt(`Enter project name${projectNames.length + 1}`);
+  projectNames.push(input);
+  const el = document.getElementById(`project${projectNames.length}`);
+  el.innerText = input;
+}
